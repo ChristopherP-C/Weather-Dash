892e1da9b9f3bb5bfd2d24136a2de6bc318584e5
ChristopherP-C <christopherp.c.2day@gmail.com>
2024-10-18 03:10:59 -0500

    created weather data routes

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -2,13 +2,30 @@ import { Router } from 'express';
 const router = Router();
 
  import HistoryService from '../../service/historyService.js';
+import weatherService from '../../service/weatherService.js';
 // import WeatherService from '../../service/weatherService.js';
 
 // TODO: POST Request with city name to retrieve weather data
 router.post('/', (req, res) => {
-  const {city} = req.body;
-  // TODO: GET weather data from city name
-  // TODO: save city to search history
+  const { city } = req.body;
+  if (req.body) {
+    // TODO: GET weather data from city name
+    router.get(`/weather/:city`, req, res) => {
+      weatherService.getWeatherForCity(city).then((data) => res.json(data));
+    };
+    // TODO: save city to search history
+    router.get(`/:city`, async (_req, res) => {
+      const data = req.params.city;
+      const weatherData = await weatherService.getWeatherForCity(data);
+      const history = await HistoryService.addCity(data);
+      if (typeof data === 'string') {
+        res.status(404).json({ message: 'No events found' });
+      } else {
+        res.json(data);
+      }
+    });
+
+  };
 });
 
 // TODO: GET search history
